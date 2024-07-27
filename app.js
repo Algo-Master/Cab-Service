7846c703735b4b82ed0c675d76d50ff520071b1a
Pratham <rimo.coder@gmail.com>
2024-07-28 01:18:05 +0530

    Map loading in also markers added and also handled user_disconnected status

--- app.js
+++ app.js
@@ -10,12 +10,26 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send("Hello World!!!");
+// WebSocket Connection handling
+io.on("connection", (socket) => {
+  console.log(`New user with id ${socket.id} Connected!!`);
+  
+  socket.on("user_location", (data) => {
+    io.emit("current_location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user_disconnected", socket.id);
+  });
+});
+
+// Express Handling HTTP Requests
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 server.listen(5000, () => {
-    console.log("Server is running on port: 5000!!");
+  console.log("Server is running on port: 5000!!");
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+console.log("Hi from Frontend");
 const socket = io();
 
 if (navigator.geolocation) {
@@ -7,7 +8,7 @@ if (navigator.geolocation) {
       socket.emit("user_location", { latitude, longitude });
     },
     (error) => {
-        console.error("Error in client script: ",error);
+      console.error("Error in client script: ", error);
     },
     {
       enableHighAccuracy: true,
@@ -17,4 +18,29 @@ if (navigator.geolocation) {
   );
 }
 
-const map = L.map("map").setView([0, 0], 10);
+// L is provided to u by Leaflet and in the setview method [latitude, longitude] -> Global Cooordinates , 10 -> Degree of Zooming
+const map = L.map("map").setView([0, 0], 17);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Cab-Service",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("current_location", (data => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+
+    if(markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+}));
+
+socket.on("user_disconnected", (id) => {
+    if(markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    };
+});
